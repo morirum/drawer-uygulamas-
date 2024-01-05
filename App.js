9bc48af93005dbf34fb259f0fe43820bc845292f
rumeysa yakut <131006594+morirum@users.noreply.github.com>
2024-01-05 19:46:33 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { NavigationContainer} from "@react-navigation/native";
+import { DrawerNavigator } from "./src/navigation/DrawerNavigator";
+
+function App(){
+    return(
+        <NavigationContainer>
+            <DrawerNavigator/>
+        </NavigationContainer>
+    );
+}
+export default App; 
